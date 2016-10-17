0c1f97abe154e636c936f71f9b0bc86c06503756
quinlan-alys <shannonqad@gmail.com>
2016-10-17 15:45:18 -0700

    beautify code

--- js/main.js
+++ js/main.js
@@ -1,56 +1,53 @@
 
 $(function () {
-  
-  $(".dropdown").on("change", function (event) {
+
+  $('.dropdown').on('change', function (event) {
     event.preventDefault();
-    $(".loader").show();
+    $('.loader').show();
 
-    
-   $( "img" ).removeClass( "logo", 2000, "easeOutQuad").addClass( "newLogo", 2000, "easeOutQuad");
 
-    var e = $(".dropdown").val();
+    $('img').removeClass('logo').addClass('newLogo');
+
+    var e = $('.dropdown').val();
     $.ajax({
-     method: "GET",
-     url: "http://api.nytimes.com/svc/topstories/v1/" + e + ".json?api-key=98e823a3d9954d40bd51888d331553cf",
-     }).done(function (data) {
-        $('.content').empty();
+      method: 'GET',
+      url: 'http://api.nytimes.com/svc/topstories/v1/' + e + '.json?api-key=98e823a3d9954d40bd51888d331553cf',
+    }).done(function (data) {
+      $('.content').empty();
       var content = '';
       //debugger;
-      
+
       var info = data.results
-      
-      .filter(function(e){
-        return e.multimedia.length
-      })
-        .splice(0,12);
 
-      $.each(info, function(key, value){
-        if (value.multimedia.length >= 5)
-         {
+        .filter(function (e) {
+          return e.multimedia.length
+        })
+        .splice(0, 12);
 
-         
-        console.log(value.abstract);
-        console.log(value.multimedia[4].url);
-        content += '<li>';
-          content +='<a href=' + value.url + '>';
+      $.each(info, function (key, value) {
+        if (value.multimedia.length >= 5) {
+
+
+
+          content += '<li>';
+          content += '<a href=' + value.url + '>';
           content += '<p class="abstract">' + value.abstract + '</p>';
-          content +='<div class="images" style ="background-image: url(' + value.multimedia[4].url + ')" ></div>';  
+          content += '<div class="images" style ="background-image: url(' + value.multimedia[4].url + ')" ></div>';
           content += '</a></li>';
-              }
+        }
       })
-      
-     $('.content').append(content);
-      
-    })
-    .always(function(){
-      $('.loader').hide()
-    })
 
-      .fail(function() {
-      $('.error').show();
-   
-    });
+      $('.content').append(content);
+
+    })
+      .always(function () {
+        $('.loader').hide()
+      })
+
+      .fail(function () {
+        $('.error').show();
+
+      });
   });
 });
- // });
-//});
+
