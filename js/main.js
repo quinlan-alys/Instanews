13424c106532a2261771cb2c9a3f499dc36cb318
justinl-y <j@solaris.org.uk>
2016-10-20 18:39:20 -0700

    JS refactored to be more readable

--- js/main.js
+++ js/main.js
@@ -1,34 +1,30 @@
 
 $(function () {
-
   $('.dropdown').on('change', function (event) {
     event.preventDefault();
+
     $('.loader').show();
-
-
     $('img').removeClass('logo').addClass('newLogo');
 
     var e = $('.dropdown').val();
+
     $.ajax({
       method: 'GET',
       url: 'https://api.nytimes.com/svc/topstories/v2/' + e + '.json?api-key=98e823a3d9954d40bd51888d331553cf',
     }).done(function (data) {
       $('.content').empty();
+      
       var content = '';
-      //debugger;
-
       var info = data.results
 
-        .filter(function (e) {
-          return e.multimedia.length
-        })
-        .splice(0, 12);
+      .filter(function (e) {
+        return e.multimedia.length
+      })
+
+      .splice(0, 12);
 
       $.each(info, function (key, value) {
         if (value.multimedia.length >= 5) {
-
-
-
           content += '<li>';
           content += '<a href=' + value.url + '>';
           content += '<p class="abstract">' + value.abstract + '</p>';
@@ -38,16 +34,11 @@ $(function () {
       })
 
       $('.content').append(content);
-
-    })
-      .always(function () {
+    }).always(function () {
         $('.loader').hide()
-      })
-
-      .fail(function () {
+    }).fail(function () {
         $('.error').show();
-
-      });
+    });
   });
 });
 
