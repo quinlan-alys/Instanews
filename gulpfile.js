241790d7c321921b5462a6a02cbe2ec4808df396
quinlan-alys <shannonqad@gmail.com>
2016-11-24 13:19:47 -0800

    add heapbox and style select menu

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,11 @@ var plumber = require('gulp-plumber');
 var coffee = require('gulp-coffee');
 var sass = require('gulp-sass');
 var reload = browsersync.reload;
-var eslint = require('gulp-eslint');
+//var eslint = require('gulp-eslint');
 var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
 var notify = require('gulp-notify');
+var babel = require('gulp-babel');
 //sass task
 
 var plumberErrorHandler={
@@ -32,30 +33,25 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('./build/css'));
 });
 
-
-gulp.task('eslint', function() {
-  return gulp.src('js/*.js')
-  .pipe(eslint())
-  .pipe(eslint.format())
-  // Brick on failure to be super strict
-  .pipe(eslint.failOnError());
-});
- 
-gulp.task('default', ['eslint'], function () {
-    console.log('heeey')
-});
+// gulp.task('default', ['eslint'], function () {
+//     console.log('heeey')
+// });
  
 
-gulp.task('plumba', function (){
-gulp.src('./src/*.ext')
-    .pipe(plumber())
-    .pipe(coffee())
-    .pipe(gulp.dest('./dist'))
+// gulp.task('plumba', function (){
+// gulp.src('./src/*.ext')
+//     .pipe(plumber())
+//     .pipe(gulp.dest('./dist'))
 
-});
+// });
  
  gulp.task('serve', function(){ //runs a take that we named default
    gulp.src('./js/*.js') //what files do we want to be absorbed by gulp
+      .pipe(babel())
+      //.pipe(eslint())
+      //.pipe(eslint.format())
+    // Brick on failure to be super strict
+      .pipe(eslint.failOnError())
       .pipe(uglify()) // enable uglify, minifies... pipe chains things together
       .pipe(rename({ extname: '.min.js' })) //  give new name to uglify file to .js
       .pipe(gulp.dest('./build/js')) // location to put results? build file.
@@ -75,16 +71,16 @@ gulp.task('browsersync', function () {
 });
 
 gulp.task('watch', function() {
-   gulp.watch('js/*.js', ['scripts', 'plumba', 'sass',]);
+   gulp.watch('js/*.js', ['scripts', 'sass',]);
    gulp.watch('sass/*.scss', ['sass'])
 });
 
 gulp.task('watch', function() {
-   gulp.watch('js/*.js', ['serve', 'eslint']);
+   gulp.watch('js/*.js', ['serve' ]); //eslint
    gulp.watch('sass/*.scss', ['sass'])
 });
 
-gulp.task('default', [ 'browsersync', 'serve', 'watch', 'sass', 'eslint']);
+gulp.task('default', [ 'browsersync', 'serve', 'watch', 'sass']);
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,48 +1,51 @@
 
 $(function () {
-  $('.dropdown').on('change', function (event) {
-    event.preventDefault();
 
-<<<<<<< HEAD
-  $('img').removeClass('logo').addClass('newLogo');
-=======
-    $('.loader').show();
-    $('img').removeClass('logo').addClass('newLogo');
->>>>>>> 448bacdfebc80bb36549c009520981a0ce5deeda
 
-    var e = $('.dropdown').val();
+  $('#mySelect').heapbox({'onChange': function () {
+  //event.preventDefault();
 
-    $.ajax({
-      method: 'GET',
-      url: 'https://api.nytimes.com/svc/topstories/v2/' + e + '.json?api-key=98e823a3d9954d40bd51888d331553cf',
-    }).done(function (data) {
-      $('.content').empty();
-      
-      var content = '';
-      var info = data.results
+  //$("#mySelect").heapbox();
+ //$('#mySelect').heapbox({'onChange':function(){
+    
 
-      .filter(function (e) {
-        return e.multimedia.length
-      })
+      $('.loader').show();
+      $('img').removeClass('logo').addClass('newLogo');
 
-      .splice(0, 12);
+      let topic = $('#mySelect').val()
+      console.log(topic);
+      $.ajax({
+        method: 'GET',
+        url: `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=98e823a3d9954d40bd51888d331553cf`,
+      }).done(function (data) {
+        $('.content').empty();
 
-      $.each(info, function (key, value) {
-        if (value.multimedia.length >= 5) {
-          content += '<li>';
-          content += '<a href=' + value.url + '>';
-          content += '<p class="abstract">' + value.abstract + '</p>';
-          content += '<div class="images" style ="background-image: url(' + value.multimedia[4].url + ')" ></div>';
-          content += '</a></li>';
-        }
-      })
+        let content = '';
+        let info = data.results
 
-      $('.content').append(content);
-    }).always(function () {
+          .filter(function (e) {
+            return e.multimedia.length
+          })
+
+          .splice(0, 12);
+
+        $.each(info, function (key, value) {
+          if (value.multimedia.length >= 5) {
+            content += '<li>';
+            content += '<a href=' + value.url + '>';
+            content += '<p class="abstract">' + value.abstract + '</p>';
+            content += '<div class="images" style ="background-image: url(' + value.multimedia[4].url + ')" ></div>';
+            content += '</a></li>';
+          }
+        })
+
+        $('.content').append(content);
+      }).always(function () {
         $('.loader').hide()
-    }).fail(function () {
+      }).fail(function () {
         $('.error').show();
-    });
+      });
+    }
   });
 });
 
